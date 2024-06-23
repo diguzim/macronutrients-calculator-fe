e4c106fbda350fb3cf9a13f8699327f7c7e13d05
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2024-06-23 16:50:21 -0300

    Starting fetch of raw ingredients from backend but still in localhost

--- src/app/raw-ingredients/page.tsx
+++ src/app/raw-ingredients/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { RawIngredient } from "../../common/interfaces/raw-ingredient.interface";
+
 export default function Page() {
+  const [rawIngredients, setRawIngredients] = useState([] as RawIngredient[]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/nutritional-entities/get-all")
+      .then((response) => response.json())
+      .then((nutritionalGroups: any[]) => {
+        const rawIngredients = nutritionalGroups.find(
+          (nutritionalGroup) => nutritionalGroup.type === "raw-ingredient"
+        ).values;
+
+        setRawIngredients(rawIngredients);
+      })
+      .catch((error) => {
+        console.error("Error fetching raw ingredients", error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Raw Ingredients</h1>
+      <ul>
+        {rawIngredients.map((rawIngredient) => (
+          <li key={rawIngredient.id}>{rawIngredient.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
