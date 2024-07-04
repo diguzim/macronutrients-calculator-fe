271be2d7c16d6914d963ed9ba0d34fc28cb57809
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2024-07-04 00:23:31 -0300

    Adjusted topbar to tailwind

--- src/components/topbar/authentication-container.tsx
+++ src/components/topbar/authentication-container.tsx
@@ -1,9 +1,8 @@
 import Button from "@mui/material/Button";
-import styles from "./authentication-container.module.css";
 
 export default function AuthenticationContainer() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-row items-center justify-between gap-5 mr-5">
       <Button variant="outlined" size="large">
         Login
       </Button>
--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -1,13 +1,12 @@
 import PercentOutlinedIcon from "@mui/icons-material/PercentOutlined";
 
 import AuthenticationContainer from "./authentication-container";
-import styles from "./topbar.module.css";
 
 export default function Topbar() {
   return (
-    <div className={styles.container}>
-      <div className={styles.logoContainer}>
-        <div className={styles.logoTitle}>
+    <div className="flex flex-row items-center justify-between bg-gray-50">
+      <div className="bg-blue-600 p-2 text-white">
+        <div className="flex flex-row items-center gap-1">
           <PercentOutlinedIcon />
           <h1>MaCal</h1>
           <PercentOutlinedIcon />
