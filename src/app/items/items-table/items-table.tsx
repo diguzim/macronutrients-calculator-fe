b763d3d891e8ab3ffaeffdcc6fefedc18c5d3cf6
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2024-07-11 03:49:03 -0300

    Using MUI table on items page

--- src/app/items/items-table/items-table.tsx
+++ src/app/items/items-table/items-table.tsx
@@ -1,6 +1,12 @@
-import { environmentVariables } from "../../../utils/environment-variables";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 
-import styles from "./items-table.module.css";
+import { environmentVariables } from "../../../utils/environment-variables";
 
 const URL = `${environmentVariables().public.backendUrl}/items`;
 
@@ -22,31 +28,33 @@ export default async function ItemsTable() {
   const items = await fetchItems();
 
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Type</th>
-          <th>Kcal (100g)</th>
-          <th>Carbohydrates</th>
-          <th>Protein</th>
-          <th>Fat</th>
-          <th>Fiber</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item) => (
-          <tr key={item.id}>
-            <td>{item.name}</td>
-            <td>{item.type}</td>
-            <td>{item.kcalPerGram}</td>
-            <td>{item.carbohydrateRatio}</td>
-            <td>{item.proteinRatio}</td>
-            <td>{item.fatRatio}</td>
-            <td>{item.fiberRatio}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table size="small" aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Kcal (100g)</TableCell>
+            <TableCell>Carbohydrates</TableCell>
+            <TableCell>Protein</TableCell>
+            <TableCell>Fat</TableCell>
+            <TableCell>Fiber</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {items.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.type}</TableCell>
+              <TableCell>{item.kcalPerGram}</TableCell>
+              <TableCell>{item.carbohydrateRatio}</TableCell>
+              <TableCell>{item.proteinRatio}</TableCell>
+              <TableCell>{item.fatRatio}</TableCell>
+              <TableCell>{item.fiberRatio}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
