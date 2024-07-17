bc2b65be0d13cae22613b0f4e5c06281133a2520
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2024-07-17 17:55:32 -0300

    feat: Improving login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
+import Header from "../../components/header/header";
 import LoginForm from "./login-form";
 
 export default function RegisterPage() {
   return (
-    <div className="bg-white">
+    <div className="bg-white flex flex-col items-center justify-center gap-10">
+      <Header size={1}>Login to MaCal</Header>
       <LoginForm />
+      <div className="flex flex-row gap-2 justify-center">
+        {"Don't have an account?"}
+        <Link href="/register" className="text-primary">
+          Register now
+        </Link>
+      </div>
     </div>
   );
 }
