138214b2dfd824a869b6c428e6235b9a185f0663
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2024-07-18 01:19:59 -0300

    feat: food search working

--- src/app/foods/food-search/food-search.tsx
+++ src/app/foods/food-search/food-search.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import SearchIcon from "@mui/icons-material/Search";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
+import { Item } from "../../../common/interfaces/item.interface";
 import Button from "../../../components/button/button";
 import FormInput from "../../../components/form-input/form-input";
+import { environmentVariables } from "../../../utils/environment-variables";
+import SearchResults from "./search-results";
 
 type FormData = {
   search: string;
@@ -14,17 +17,31 @@ const initialFormData: FormData = {
   search: "",
 };
 
+const URL = `${environmentVariables().public.backendUrl}/items?`;
+
 export default function FoodSearch() {
   const { control, handleSubmit, reset } = useForm<FormData>({
     defaultValues: initialFormData,
   });
+  const [foods, setFoods] = useState([] as Item[]);
 
   const onSubmit = useCallback(async (data: FormData) => {
-    console.log(data);
+    const urlWithQuery =
+      URL + new URLSearchParams({ name: data.search }).toString();
+
+    const response = await fetch(urlWithQuery, {
+      next: {
+        tags: ["items"],
+      },
+    });
+
+    const items = await response.json();
+
+    setFoods(items);
   }, []);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-10">
       <form
         className="flex flex-row gap-2 max-w-96 self-center"
         onSubmit={handleSubmit(onSubmit)}
@@ -39,7 +56,7 @@ export default function FoodSearch() {
           <SearchIcon />
         </Button>
       </form>
-      <p>Here shall be the result of the search</p>
+      <SearchResults items={foods} />
     </div>
   );
 }
