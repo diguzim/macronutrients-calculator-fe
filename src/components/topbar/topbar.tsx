368e2e1510b5ff9be30adf27e7e3a8cc4b43c5b9
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2024-07-21 04:24:48 -0300

    feat: using semantic nav in topbar component

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -3,9 +3,9 @@ import GeneralContainer from "./general-container";
 
 export default function Topbar() {
   return (
-    <div className="flex flex-row items-center justify-between py-2 px-6 border-b-2">
+    <nav className="flex flex-row items-center justify-between py-2 px-6 border-b-2">
       <GeneralContainer />
       <AuthenticationContainer />
-    </div>
+    </nav>
   );
 }
