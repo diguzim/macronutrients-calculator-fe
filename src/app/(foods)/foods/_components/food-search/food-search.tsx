8bb84f7df600308519e76a55fe1087af84566f96
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2024-07-28 16:56:55 -0300

    feat: integrating food search form with query parameter

--- src/app/(foods)/foods/_components/food-search/food-search.tsx
+++ src/app/(foods)/foods/_components/food-search/food-search.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import SearchIcon from "@mui/icons-material/Search";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useSnackbar } from "notistack";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Item } from "../../../../../common/interfaces/item.interface";
 import Button from "../../../../../components/button/button";
 import FormInput from "../../../../../components/form-input/form-input";
 import LinearProgress from "../../../../../components/linear-progress/linear-progress";
 import theme from "../../../../../theme/theme";
+import { ROUTES } from "../../../../../utils/constants/routes";
 import { environmentVariables } from "../../../../../utils/environment-variables";
 import SearchResults from "./search-results";
 
@@ -24,24 +25,23 @@ const initialFormData: FormData = {
 const URL = `${environmentVariables().public.backendUrl}/items/search?`;
 
 export default function FoodSearch() {
-  const { control, handleSubmit, formState } = useForm<FormData>({
+  const { control, handleSubmit, setValue } = useForm<FormData>({
     defaultValues: initialFormData,
   });
-  const [foods, setFoods] = useState([] as Item[]);
+  const [foods, setFoods] = useState(null as Item[] | null);
   const sx = { color: theme.colors.primary.contrast };
   const { enqueueSnackbar } = useSnackbar();
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
   const searchParams = useSearchParams();
-  const searchTerm = searchParams.get("search");
-  console.log("searchTerm", searchTerm);
+  const incomingSearchTerm = searchParams.get("search");
 
-  const { isSubmitted, isSubmitting } = formState;
+  const fetchFoods = useCallback(
+    async (name: string) => {
+      const urlWithQuery = URL + new URLSearchParams({ name }).toString();
 
-  const onSubmit = useCallback(
-    async (data: FormData) => {
-      const urlWithQuery =
-        URL + new URLSearchParams({ name: data.search }).toString();
-
+      setIsLoading(true);
       const response = await fetch(urlWithQuery, {
         next: {
           tags: ["items"],
@@ -51,14 +51,29 @@ export default function FoodSearch() {
       if (!response.ok) {
         enqueueSnackbar("Error searching foods", { variant: "error" });
         return;
+      } else {
+        const items = await response.json();
+
+        setFoods(items);
       }
-
-      const items = await response.json();
-
-      setFoods(items);
+      setIsLoading(false);
     },
     [enqueueSnackbar]
   );
+
+  const onSubmit = useCallback(
+    async (data: FormData) => {
+      router.push(`${ROUTES.FOODS}?search=${data.search}`);
+    },
+    [router]
+  );
+
+  useEffect(() => {
+    if (incomingSearchTerm) {
+      setValue("search", incomingSearchTerm);
+      fetchFoods(incomingSearchTerm);
+    }
+  }, [fetchFoods, incomingSearchTerm, setValue]);
 
   return (
     <div className="flex flex-col gap-10">
@@ -71,13 +86,14 @@ export default function FoodSearch() {
           name="search"
           label="Search"
           size="small"
+          required
         />
         <Button type="submit" size="small">
           <SearchIcon sx={sx} />
         </Button>
       </form>
-      {isSubmitting && <LinearProgress />}
-      {!isSubmitting && isSubmitted && <SearchResults items={foods} />}
+      {isLoading && <LinearProgress />}
+      {foods && <SearchResults items={foods} />}
     </div>
   );
 }
